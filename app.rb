37080fb7d3cd3ff2eacd30b5d38a66fb8e220970
tatakami <mitti1983@yahoo.co.jp>
2017-05-18 17:53:31 +0900

    heroku-api

--- app.rb
+++ app.rb
@@ -165,6 +165,11 @@ get "/herokus" do
   @heroku_api = Heroku::API.new(:api_key => 'c7283065-0c22-40ee-a227-939559be0bad')
   @apps = @heroku_api.get_apps.body
 
+  @hoge = RestClient.get('https://api.status.salesforce.com/v1/instances/AP0/status')
+  logger.info('----------Salesforce AP0 status start--------------------')
+  logger.info(@hoge)
+  logger.info('----------Salesforce AP0 status end--------------------')
+
 =begin
   @apps.each do |app|
     app_info = @heroku_api.get_app(app['name'])
@@ -198,7 +203,7 @@ get "/sfs" do
 
   @hoge = RestClient.get('https://api.status.salesforce.com/v1/instances/AP0/status')
   #logger.info('----------Salesforce AP0 status start--------------------')
-  #logger.info(@response)
+  #logger.info(@hoge)
   #logger.info('----------Salesforce AP0 status end--------------------')
 
   erb :index_sfs
