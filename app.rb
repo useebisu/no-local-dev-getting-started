667e6cdd3afe3cf380e09e49dc4e566b89f4a3e8
tatakami <mitti1983@yahoo.co.jp>
2017-05-18 15:08:45 +0900

    heroku-api

--- app.rb
+++ app.rb
@@ -165,10 +165,10 @@ get "/herokus" do
   @heroku_api = Heroku::API.new(:api_key => 'c7283065-0c22-40ee-a227-939559be0bad')
   @apps = @heroku_api.get_apps.body
 
-  @test = RestClient.get('https://api.status.salesforce.com/v1/instances/AP0/status')
+  @response = RestClient.get('https://api.status.salesforce.com/v1/instances/AP0/status')
   logger.info('----------Salesforce AP0 status--------------------')
-  logger.info(@test.inspect)
-  logger.info(@test.class)
+  logger.info(@response.body)
+  logger.info(@response.class)
 
 =begin
   @apps.each do |app|
